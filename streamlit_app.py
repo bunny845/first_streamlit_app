58b5c049acd7436241d4d96806c6234626c0a4cd
Deekshith kumar Vobilisetty <61815031+bunny845@users.noreply.github.com>
2023-05-31 16:55:48 +0530

    set fruit col as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,11 @@ streamlit.text('🐔 Hand-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 # new header 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 # use pandas to fetch data from a csv file
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# set fruit col as index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
